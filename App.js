8440ea30f8a92ec8baff8d49ff88df223c46bdd5
altafmalik555786 <altafmalik555786@gmail.com>
2022-01-28 12:18:59 +0500

    final completed

--- App.js
+++ App.js
@@ -1,57 +1,53 @@
 import React from 'react';
 import {
-    StyleSheet,
-    View,
-    Text,
-    Button,
-    Image,
-    TouchableOpacity
+  StyleSheet,
+  View,
+  Text
 } from 'react-native';
-import Dashboard from './components/book'
+import HomePage from './components/book'
 const styles = StyleSheet.create({
   welcome: {
     textAlign: 'center',
     marginBottom: "50px",
-    fontSize:'26px'
+    fontSize: '26px'
   },
   logo: {
-        width: 130,
-        height: 130,
-        alignSelf:'center',
-        marginBottom: 50,     
-    },
-     button: {
-        padding: 10,
-        marginTop: 10,
-        backgroundColor: 'rgba(235, 168, 12, 0.911)',
-        width: '100%',
-        borderRadius: 4,
-    },
-    buttonText: {
-        textAlign: 'center',
-        color: '#fff',
-        fontSize:28,  
-    },
+    width: 130,
+    height: 130,
+    alignSelf: 'center',
+    marginBottom: 50,
+  },
+  button: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: 'rgba(235, 168, 12, 0.911)',
+    width: '100%',
+    borderRadius: 4,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 28,
+  },
 });
 
-const App= () => {
-    const gotoBooksPage = () => {
-    };
+const App = () => {
+  const gotoBooksPage = () => {
+  };
 
-const Separator = () => (
-  <View style={styles.separator} />
-);
+  const Separator = () => (
+    <View style={styles.separator} />
+  );
 
-return (
+  return (
     <View>
-     <Text style={styles.welcome}>
+      <Text style={styles.welcome}>
         <Text>{`Welcome to BOOKS APP\nRead here to get\nKnowldge`}</Text>
       </Text>
-      <Text>{'\n\n\n\n'} </Text>
-      <Text>{'\n\n\n\n'} </Text>
-         <Dashboard />
-    </View>             
-)
+
+      <HomePage />
+    </View>
+  )
 };
 
 export default App;
--- components/book.js
+++ components/book.js
@@ -3,85 +3,39 @@ import {
   Text,
   View,
   StyleSheet,
-  Image,
-  Platform,
   Button,
   Share,
-  KeyboardAvoidingView,
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  header: {
-    textAlign: 'center',
-    marginBottom: '0px',
-    fontSize: 26,
-    fontWeight: 'bold',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: 'rgba(155, 199, 120, 0.356)',
-  },
 
-  text: {
-    fontSize: 26,
-  },
-});
-const Dashboard = () => {
-  const onShare6 = async () => {
+const HomePage = () => {
+
+
+  const onBookTwo = async () => {
     {
       const result = await Share.share({
-        message:
-          'Class 6th Computer book Punjab Curriculum and Textbook Board https://co.vid19.pk/media/books/computer-education-class-6th.pdf',
-      });
-    }
-  };
-
-  const onShare7 = async () => {
-    {
-      const result = await Share.share({
-        message:
+        involve:
           'Class 7th Computer book Punjab Curriculum and Textbook Board https://co.vid19.pk/media/books/computer-education-class-7th.pdf',
       });
     }
   };
 
-  const onShare8 = async () => {
+
+
+  const onBookOne = async () => {
     {
       const result = await Share.share({
-        message:
-          'Class 8th Computer book Punjab Curriculum and Textbook Board https://co.vid19.pk/media/books/computer-education-class-8th.pdf',
+        involve:
+          'Class 6th Computer book Punjab Curriculum and Textbook Board https://co.vid19.pk/media/books/computer-education-class-6th.pdf',
       });
     }
   };
 
-  const onShare9 = async () => {
-    {
-      const result = await Share.share({
-        message:
-          'Class 9th Computer book Punjab Curriculum and Textbook Board https://co.vid19.pk/media/books/computer-education-class-9th.pdf',
-      });
-    }
-  };
-
-  const onShare10 = async () => {
-    {
-      const result = await Share.share({
-        message:
-          'Class 10th Computer book Punjab Curriculum and Textbook Board https://co.vid19.pk/media/books/computer-education-class-10th.pdf',
-      });
-    }
-  };
-
-  const logout = () => {};
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>BOOKS</Text>
+    <View style={styles.main}>
+      <Text style={styles.heading}>BOOKS</Text>
       <Text>{'\n'} </Text>
-      <Image
-        source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHjspPtUIqQmZyzIoz0yzFIjQ8sR_6Edv7Aw&usqp=CAU' }}
-        style={{ width: 320, height: 450 }}
-      />
 
       <Text style={styles.text}>Computer Education Class 6th</Text>
       <Text style={styles.description}>
@@ -93,17 +47,14 @@ const Dashboard = () => {
       </a>
 
       <Button
-        onPress={onShare6}
+        onPress={onBookOne}
         title="Share"
         color="orange"
         fontWeight="bold"></Button>
       <Text>{'\n'} </Text>
-      <Image
-        source={{ uri: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fw7.pngwing.com%2Fpngs%2F903%2F111%2Fpng-transparent-book-cover-e-book-bookmark-novel-book-thumbnail.png&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dbook%2Bcover&tbnid=fxQBKKxislhhBM&vet=12ahUKEwiQktyWv771AhUhgXMKHQ6yC9UQMygGegUIARDDAQ..i&docid=M3QV-B_ny8bzrM&w=360&h=422&itg=1&q=book%20thumbnail%20image&ved=2ahUKEwiQktyWv771AhUhgXMKHQ6yC9UQMygGegUIARDDAQ' }}
-        style={{ width: 320, height: 450 }}
-      />
 
-      <Text style={styles.text}>Computer Education Class 7th</Text>
+
+      <Text style={styles.txt}>Computer Education Class 7th</Text>
       <Text style={styles.description}>
         Class 7th Computer book Punjab Curriculum and Textbook Board
       </Text>
@@ -113,75 +64,39 @@ const Dashboard = () => {
       </a>
 
       <Button
-        onPress={onShare7}
+        onPress={onBookTwo}
         title="Share"
         color="orange"
         fontWeight="bold"></Button>
       <Text>{'\n'} </Text>
-      <Image
-        source={{ uri: 'https://books-dj2.pages.dev/thumbnails/8th.jpg' }}
-        style={{ width: 320, height: 450 }}
-      />
 
-      <Text style={styles.text}>Computer Education Class 8th</Text>
-      <Text style={styles.description}>
-        Class 8th Computer book Punjab Curriculum and Textbook Board
-      </Text>
 
-      <a href="https://co.vid19.pk/media/books/computer-education-class-8th.pdf">
-        <h2>Read OR Download</h2>
-      </a>
-
-      <Button
-        onPress={onShare8}
-        title="Share"
-        color="orange"
-        fontWeight="bold"></Button>
-      <Text>{'\n'} </Text>
-      <Image
-        source={{ uri: 'https://books-dj2.pages.dev/thumbnails/9th.jpg' }}
-        style={{ width: 320, height: 450 }}
-      />
-
-      <Text style={styles.text}>Computer Education Class 9th</Text>
-      <Text style={styles.description}>
-        Class 9th Computer book Punjab Curriculum and Textbook Board
-      </Text>
-
-      <a href="https://co.vid19.pk/media/books/computer-education-class-9th.pdf">
-        <h2>Read OR Download</h2>
-      </a>
-
-      <Button
-        onPress={onShare9}
-        title="Share"
-        color="orange"
-        fontSize="26"
-        fontWeight="bold"></Button>
-      <Text>{'\n'} </Text>
-      <Image
-        source={{ uri: 'https://books-dj2.pages.dev/thumbnails/10th.jpg' }}
-        style={{ width: 320, height: 450 }}
-      />
-
-      <Text style={styles.text}>Computer Education Class 10th</Text>
-      <Text style={styles.description}>
-        Class 10th Computer book Punjab Curriculum and Textbook Board
-      </Text>
-
-      <a href="https://co.vid19.pk/media/books/computer-education-class-10th.pdf">
-        <h2>Read OR Download</h2>
-      </a>
-
-      <Button
-        onPress={onShare10}
-        title="Share"
-        color="orange"
-        fontSize="26"
-        fontWeight="bold"></Button>
-      <Text>{'\n'} </Text>
     </View>
   );
 };
 
-export default Dashboard;
+export default HomePage;
+
+
+const styles = StyleSheet.create({
+
+  txt: {
+    fontSize: 28,
+  },
+
+
+  main: {
+    flex: 1,
+    backgroundColor: 'rgba(155, 199, 120, 0.356)',
+  },
+
+
+  heading: {
+    textAlign: 'center',
+    marginBottom: '2px',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+
+
+});
